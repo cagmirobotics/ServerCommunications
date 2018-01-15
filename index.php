5291c88a1188092384071e91e0fe6b8fb9854163
Carlos <Carlos@LAPTOP-A77KSOVO>
2018-01-15 13:52:10 +0100

    Creación del formulario de inicio: - Selección del dispositivo - Configuración de ruta - Datos del usuario y código de validación

--- index.php
+++ index.php
@@ -4,15 +4,43 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
+<!--Datos de ruta:
+- Origen: Av. de Brasil, 1, 28020 Madrid = 40.45361,-3.694235
+- Destino: Calle de Orense, 30, 28020 Madrid = 40.453253, -3.694936-->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Formulario de ruta</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        echo 'Hola Mundo';
-        ?>
+        <form action="/MapsRequest.php">
+            
+            Seleccionar dispositivo:<br>
+            <select>
+                <option value="Dispositivo1">Dispositivo Beta</option>
+            </select>
+            <br><br>
+            
+            Datos de ruta<br>
+            Origen: Av. de Brasil, 1, 28020 Madrid<br>
+            <input type="text" name="Origen" value="40.45361,-3.694235" placeholder="Av. de Brasil, 1, 28020 Madrid">
+            <br>
+            Destino: Calle de Orense, 30, 28020 Madrid<br>
+            <input type="text" name="Destino" value="40.453253, -3.694936" placeholder="Calle de Orense, 30, 28020 Madrid">
+            <br><br>
+            
+            Datos de contacto<br>
+            Nombre:<br>
+            <input type="text" name="NombreUsuario" value="Carlos Aguilar" placeholder="Inserte el nombre del usuario">
+            <br>
+            Código de valicación:<br>
+            <input type="text" name="CodigoValidacion" value="123456" placeholder="123456">
+            <br><br>
+            
+            <input type="submit" value="Configurar">
+        </form>
+
     </body>
 </html>
